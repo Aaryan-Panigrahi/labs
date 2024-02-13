baf1f4cfd0e487139c2dcdcc16201ef5c83f73fa
Aaryan Takayuki Panigrahi <116629518+Aaryan-Panigrahi@users.noreply.github.com>
2024-02-13 13:28:24 +0530

    num_nodes_tree

---
+++ Algo/lab6_dnc/no_nodes.c
@@ -0,0 +1,39 @@
+#include "tree.h"
+
+int count_nodes(Tnode* root) {
+    if (root == NULL) {
+        return 0;
+    } else {
+        // Count nodes in the left subtree
+        int left_count = count_nodes(root->lch);
+        // Count nodes in the right subtree
+        int right_count = count_nodes(root->rch);
+        // Add 1 for the current root node
+        return left_count + right_count + 1;
+    }
+}
+
+int main() {
+    printf("Enter the key for the root:");
+    int k;
+    scanf("%d", &k);
+    Tnode* root = get_node(k);
+    
+    // root = make_tree(root);
+
+    //sample tree
+    root->lch = get_node(2);
+    root->rch = get_node(3);
+    root->lch->lch = get_node(4);
+    root->lch->rch = get_node(5);
+
+    printf("Inorder: ");
+    inorder(root);
+    printf("\nPreorder: ");
+    preorder(root);
+    printf("\nPostorder: ");
+    postorder(root);
+    printf("\n");
+    printf("\n Number of nodes: %d\n", count_nodes(root));
+    return 0;
+}
